fe8a5510f6350376c5e9ae667aad926976a7d8ad
thanukumari123 <kumarithanu324@gmail.com>
2026-04-13 20:03:42 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,6 @@
+statement=" python is a programming language"#string
+print(statement)
+statement=statement.lower()#lower str
+print(statement)
+statement=statement.upper()#upper str
+print(statement)
