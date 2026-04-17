97ebdec939d87d21089f07492b405339bb9d5721
thanukumari123 <kumarithanu324@gmail.com>
2026-04-17 11:47:06 +0530

    Add files via upload

---
+++ method.py
@@ -0,0 +1,19 @@
+fruits=['apple','orange','banana']
+fruits.append("grapes")#append method
+print(fruits)
+fruits=['apple','orange','banana']#extend method
+model=['ford','bmw','vivo']
+fruits.extend(model)
+print(fruits)
+fruits=['apple','orange','banana']
+fruits.insert(1,"grapes")#insert method
+print(fruits)
+fruits=['apple','orange','banana']
+fruits.pop()#pop method
+print(fruits)
+fruits=['apple','orange','banana']
+fruits.remove("orange")#remove method
+print(fruits)
+fruits=['apple','orange','banana']
+fruits.sort()
+print(fruits)
