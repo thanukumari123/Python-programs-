9f75a787bfe205ae3df67221fda19b2891fc7868
thanukumari123 <kumarithanu324@gmail.com>
2026-04-19 17:46:26 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,14 @@
+def ADD(a,b): 
+    c = a + b
+    return c
+print(ADD(3,9))
+
+def MUL(a,b):
+    c = a * b
+    return c
+print(MUL(3,2))
+
+def AVERAGE(a,b):
+    c = (a + b) / 2   
+    return c
+print(AVERAGE(3,9))
